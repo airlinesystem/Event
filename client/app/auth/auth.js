c05c082df6a23d6d526395ca6716b0e193fb4528
MoathDawahreh <moath.dawahreh@yahoo.com>
2017-02-13 18:37:15 +0200

    siginPage

--- client/app/auth/auth.js
+++ client/app/auth/auth.js
@@ -8,16 +8,23 @@ angular.module('event.auth', [])
   $scope.org ={};
   $scope.event = {};
 
-  // $scope.signin = function () {
-  //   Auth.signin($scope.user)
-  //     .then(function (token) {
-  //       $window.localStorage.setItem('com.shortly', token);
-  //       $location.path('/links');
-  //     })
-  //     .catch(function (error) {
-  //       console.error(error);
-  //     });
-  // };
+  $scope.userSignin = function () {
+    Auth.userSignin($scope.user)
+      .then(function (token) {
+        $window.localStorage.setItem('com.event', token);
+        $location.path('/userProfile');
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
+  $scope.OrgSignin = function(){
+    Auth.OrgSignin($scope.org)
+    .then(function(token){
+      $window.localStorage.setItem('com.event',token);
+      $location.path('/orgProfile')
+    })
+  }
 
   $scope.userSignup = function () {
     console.log(";dc;")
