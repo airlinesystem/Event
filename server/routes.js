33613726ac2255e8accf1c401ab5dc625d122837
AbobakrAlduais <aalduais@gmail.com>
2017-02-13 14:15:44 +0200

    1

--- server/routes.js
+++ server/routes.js
@@ -1,10 +1,12 @@
 var userController = require('./userController.js');
 var organizerController = require('./organizerController.js');
+var eventController = require('./eventController.js');
 // var organizerController = require('../users/userController.js')
 
 module.exports = function (app, express) {
 
 app.post('/api/OrgSignup', organizerController.signup);
 app.post('/api/userSignup', userController.userSignup);
+app.post('/api/orgProfile', eventController.addEvent);
 // app.get('/api/signedin', userController.checkAuth);
 }
