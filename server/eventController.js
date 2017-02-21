b2e7477e7a6d24da1e45aa4fd4bf6e2d66b851c1
AhmadKhasawneh <Ahmad.Khasawneh@hotmail.com>
2017-02-21 20:31:54 +0200

    Send multiple emails

--- server/eventController.js
+++ server/eventController.js
@@ -1,34 +1,13 @@
 var Event = require('./model/event.js');
 var Events = require('./collections/events');
+var Users = require('./collections/users.js');
 var jwt = require('jwt-simple');
 var nodemailer = require('nodemailer');
 
 module.exports = {
 
+
     addEvent:function (req,res) {
-      var transporter = nodemailer.createTransport({
-          service: 'gmail',
-          auth: {
-              user: 'events.planing.rbk@gmail.com', // Your email id
-              pass: 'Events.Planning' // Your password
-          }
-      });
-
-      var mailOptions = {
-        from: 'events.planning.rbk@gmail.com', // sender address
-        to: 'ahmad.khasawneh@hotmail.com', // list of receivers
-        subject: 'Email Example', // Subject line
-        text: "Hello World!"
-        //html: '<b>Hello world ✔</b>' // You can choose to send an HTML body instead
-      };
-
-      transporter.sendMail(mailOptions, function(error, info){
-        if(error){
-            console.log(error);
-        }else{
-            console.log('Message sent: ' + info.response);
-        };
-      });
     var decoded = jwt.decode(req.body.tok, 'not your bussines!!')
 
           var eventName    = req.body.eventName;
@@ -57,6 +36,40 @@ module.exports = {
             location2   : location2
           })
           .then(function(newEvent) {
+            Users
+            .reset()
+            .fetch()
+            .then(function(data) {
+                console.log(data)
+                var emails=[];
+                for(var i=0; i<data.models.length; i++){
+                  emails.push(data.models[i].attributes.email);
+                }
+                console.log(emails)
+                var transporter = nodemailer.createTransport({
+                service: 'gmail',
+                auth: {
+                user: 'events.planing.rbk@gmail.com', // Your email id
+                pass: 'Events.Planning' // Your password
+                    }
+                });
+
+                var mailOptions = {
+                  from: 'events.planning.rbk@gmail.com', // sender address
+                  to: emails, // list of receivers
+                  subject: 'Email Example', // Subject line
+                  text: "Hello World!"
+                  //html: '<b>Hello world ✔</b>' // You can choose to send an HTML body instead
+                };
+
+                transporter.sendMail(mailOptions, function(error, info){
+                  if(error){
+                      console.log(error);
+                  }else{
+                      console.log('Message sent: ' + info.response);
+                  };
+                });
+            });
             res.send(newEvent);
           });
         }
