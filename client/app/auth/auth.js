9da5d09027803bb06103b2b717dd331c46312fd7
AhmadKhasawneh <Ahmad.Khasawneh@hotmail.com>
2017-02-22 22:47:42 +0200

    Style editing

--- client/app/auth/auth.js
+++ client/app/auth/auth.js
@@ -1,18 +1,13 @@
-
 angular.module('event.auth', [])
 
 .controller('AuthController', function ($scope, $window, $location, Auth) {
+
   $scope.user = {};
   $scope.org = {};
   $scope.event = {};
   $scope.why ={}
   $scope.why2 ={};
-  $scope.eventType =[
-  "Music",
-  "Tech",
-  "Sport"
-  ];
-  
+  $scope.eventType =["Music","Tech","Sport"];
 
   $scope.logout = function () {
     Auth.signout()
@@ -20,7 +15,6 @@ angular.module('event.auth', [])
 
   $scope.userSignin = function () {
     var temp=$scope.user
-
     Auth.userSignin(temp)
     .then(function (token) {
       $window.localStorage.setItem('com.event', token);
@@ -30,8 +24,8 @@ angular.module('event.auth', [])
       console.error(error);
     });
   };
+
   $scope.OrgSignin= function(){
-    
     var temp=$scope.org;
     Auth.OrgSignin(temp)
     .then(function(token){
@@ -41,10 +35,9 @@ angular.module('event.auth', [])
     .catch(function (error) {
       console.error(error);
     });
-
   }
+
   $scope.userSignup = function () {
-    
     var temp=$scope.user
     Auth.userSignup(temp)
     .then(function (token) {
@@ -56,20 +49,20 @@ angular.module('event.auth', [])
       console.error(error);
     });
   };
+
   $scope.OrgnizerSignup = function () {
-   
     var temp=$scope.org
     Auth.OrgSignup(temp)
     .then(function (token) {
-     $window.localStorage.setItem('com.event', token);
-     Auth.getOrgEvent($window.localStorage.getItem('com.event', token))
-     
-     $location.path('/orgProfile');
-   })
+      $window.localStorage.setItem('com.event', token);
+      Auth.getOrgEvent($window.localStorage.getItem('com.event', token))
+      $location.path('/orgProfile');
+    })
     .catch(function (error) {
       console.error(error);
     });
   };
+
   $scope.CreateEvent = function () {
     var temp=$scope.event
     temp.location2 = $window.localStorage.location2;
@@ -88,12 +81,10 @@ angular.module('event.auth', [])
   $scope.maps= function(location, id) {
     location = location.split(',')
     var amman = { lat: parseFloat(location[0].substr(1)), lng: parseFloat(location[1].substr(0, location[1].length-1)) };
-
     var map = new google.maps.Map(document.getElementById(id), {
       zoom: 15,
       center: amman
     });
-
     var marker = new google.maps.Marker({
       position: amman,
       map: map,
@@ -106,15 +97,10 @@ angular.module('event.auth', [])
      $scope.why.events = data;
    })
     
-    
     Auth.getUserEvent($window.localStorage.getItem('com.event')).then(function (data) {
      $scope.why2.events = data;
    })
-
-    
-    
   } 
   $scope.bring();
-
 
 });
